7f944192a2422f244f1dc297e0658703c03d70c1
Anton Kazantsev <146850404+TuchkaSoul@users.noreply.github.com>
2023-12-19 20:55:58 +0500

    Add files via upload

---
+++ FunctionSystemCompleteness/FunctionSystemCompleteness/Program.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Полнота системы функций");
+        CheckSystem("S0.txt");
+        Console.WriteLine("\n\n\n");
+        for (int i = 1;i<=4;i++) 
+        {
+            string path = "S" + i.ToString() + ".txt";
+            CheckSystem(path);
+            Console.WriteLine("\n\n\n");
+        }
+        
+
+    }
+    public static void CheckSystem(string path)
+    {
+        string[] FunctionSystem= ReadIntoFile(path);
+        if (FunctionSystem.Length == 0) { Console.WriteLine("Недостаточно функций в системе..."); return; }
+        Console.WriteLine("\nЕсли желаете отобразить таблицы истиности текущей системы нажмите Enter.\n");
+        if (Console.ReadKey(true).Key == ConsoleKey.Enter)
+        {
+            int i = 1;
+            foreach (var item in FunctionSystem)
+            {
+                Console.WriteLine("F" + (i++));
+                PrintTruthTable(item);
+            }
+        }
+        bool[,] ClassTab = GetTableClassPosta(FunctionSystem);
+        Console.WriteLine(path);
+        ShowClassPosta(ClassTab);
+        if (CheckFullness(ClassTab))
+            Console.WriteLine("[ Система функций полная ]");
+        else Console.WriteLine("[ Система функций неполная ]");
+        Console.WriteLine("\nНажмите любую кнопку для продолжения.\nДля выхода нажмите esc");
+        if (Console.ReadKey(true).Key == ConsoleKey.Escape) Environment.Exit(0);
+    }
+    public static bool CheckFullness(bool[,] CoverTab)
+    {
+        bool isCheck = true;
+        for (int i = 0; i < CoverTab.GetLength(1); i++)
+        {
+            isCheck = true;
+            for (int j = 0; j < CoverTab.GetLength(0)&&isCheck; j++)
+            {
+                isCheck = isCheck&&CoverTab[j,i];
+            }
+            if (isCheck) return false;
+        }
+        return true;
+    }
+    public static void ShowClassPosta(bool[,] CoverTab)
+    {
+        Console.Write("\nПринадлежность функций к классам Поста:\n" + new string('_', 38));
+        Console.Write($"\n|      |  T0 |  T1 |  S  |  M  |  L  |\n" + new string('-', 38));
+        
+        for (int i = 0; i < CoverTab.GetLength(0); i++)
+        {
+            Console.Write($"\n| F" + (i + 1) + new string(' ', 4 - (i + 1).ToString().Length) + "|");
+            for (int j = 0; j < CoverTab.GetLength(1); j++)
+            {
+                if (CoverTab[i,j])
+                    Console.Write($"  +  |");
+                else Console.Write($"     |");
+            }
+
+        }
+        Console.WriteLine("\n" + new string('-', 38));
+
+    }
+    public static bool[,] GetTableClassPosta(string[] FunctionSystem)
+    {
+        bool[,] TableClass = new bool[FunctionSystem.Length, 5];
+        for (int i = 0; i < TableClass.GetLength(0); i++)
+        {
+            int j = 0;
+            TableClass[i, j++] = CheckConstZero(FunctionSystem[i]);
+            TableClass[i,j++]= CheckConstOne(FunctionSystem[i]);
+            TableClass[i,j++]= CheckSelfsimilarity(FunctionSystem[i]);
+            TableClass[i,j++]= CheckMonotony(FunctionSystem[i]);
+            TableClass[i, j] = CheckLinearity(FunctionSystem[i]);
+        }
+        return TableClass;
+
+    }
+    public static void PrintTruthTable(string vector)
+    {
+        Console.WriteLine("Полученый вектор функции: " + vector);
+        int i = 0;
+        int len = vector.Length;
+        Console.WriteLine($"Таблица истиности\n---------------------");
+        switch (len)
+        {
+            case 1:
+                Console.WriteLine($"| F | ");
+                Console.WriteLine($"-----");
+                Console.WriteLine($"| {vector[i++]} | ");
+                Console.WriteLine(new string('-', 5) + "\n\n");
+                return;
+            case 2:
+                Console.WriteLine($"| x |   | F | ");
+                Console.WriteLine(new string('-', 13 + len - 2));
+                break;
+            case 4:
+                Console.WriteLine($"| x | y |   | F | ");
+                Console.WriteLine(new string('-', 13 + len));
+                break;
+            case 8:
+                Console.WriteLine($"| x | y | z |   | F | ");
+                Console.WriteLine(new string('-', 13 + len));
+                break;
+        }
+
+        for (int x = 0; x <= 1 && len >= 2; x++)
+        {
+            for (int y = 0; y <= 1 && len >= 4; y++)
+            {
+                for (int z = 0; z <= 1 && len == 8; z++)
+                {
+                    Console.WriteLine($"| {x} | {y} | {z} |   | {vector[i++]} | ");
+                }
+                if (len == 4)
+                {
+                    Console.WriteLine($"| {x} | {y} |   | {vector[i++]} | ");
+                }
+            }
+            if (len == 2)
+            {
+                Console.WriteLine($"| {x} |   | {vector[i++]} | ");
+                Console.WriteLine(new string('-', 13 + len - 2) + "\n\n");
+                return;
+
+            }
+        }
+
+        Console.WriteLine(new string('-', 13 + len) + "\n\n");
+    }
+    public static bool CheckConstZero(string vector)
+    {
+
+        if (vector[0] == '0')
+            return true;
+        else return false;
+    }
+    public static bool CheckConstOne(string vector)
+    {
+        if (vector[vector.Length - 1] == '1')
+            return true;
+        else return false;
+    }
+    public static bool CheckSelfsimilarity(string vector)
+    {
+        if (vector.Length == 1) return false;
+        bool isCheck = true;
+        int len = vector.Length;
+        for (int i = 0; i < len / 2 && isCheck; i++)
+            isCheck = isCheck && vector[i] != vector[vector.Length - 1 - i];
+        return isCheck;
+
+    }
+    public static bool CheckMonotony(string vector)
+    {
+        if (vector.Length == 1) return true;
+        bool isCheck = true;
+        int len = vector.Length;
+        switch (len)
+        {
+            case 2:
+                if (vector[0] <= vector[1])
+                    return true;
+                else return false;
+            case 4:
+                for (int i = 0; i < len && isCheck; i += 2)
+                {
+                    isCheck = isCheck && vector[i] <= vector[i + 1];
+                }
+                isCheck = isCheck && vector.Substring(0, len / 2).CompareTo(vector.Substring(len / 2)) != 1;
+                return isCheck;
+            case 8:
+                for (int i = 0; i < len && isCheck; i += 2)
+                {
+                    isCheck = isCheck && vector[i] <= vector[i + 1];
+                }
+                for (int i = 0; i < len && isCheck; i += 4)
+                {
+                    isCheck = isCheck && vector.Substring(i, len / 4).CompareTo(vector.Substring(i + len / 4, len / 4)) != 1;
+                }
+                isCheck = isCheck && vector.Substring(0, len / 2).CompareTo(vector.Substring(len / 2, len / 2)) != 1;
+                return isCheck;
+        }
+        return false;
+
+    }
+    public static bool CheckLinearity(string vector)
+    {
+        if (vector.Length == 1 || vector.Length == 2) return true;
+
+        int len = vector.Length;
+        int[] polinom = new int[len];
+        switch (len)
+        {
+            case 4:
+                polinom[0] = int.Parse(vector[0].ToString());
+                polinom[1] = int.Parse(vector[2].ToString()) == 0 ? polinom[0] : polinom[0] == 0 ? 1 : 0;
+                polinom[2] = int.Parse(vector[1].ToString()) == 0 ? polinom[0] : polinom[0] == 0 ? 1 : 0;
+                polinom[3] = int.Parse(vector[3].ToString()) == 0 ? (polinom[0] + polinom[1] + polinom[2]) % 2 == 0 ? 0 : 1 : (polinom[0] + polinom[1] + polinom[2]) % 2 == 1 ? 0 : 1;
+                if (polinom[3] == 1)
+                    return false;
+                else return true;
+            case 8:
+                polinom[0] = int.Parse(vector[0].ToString());
+
+                polinom[1] = int.Parse(vector[4].ToString()) == 0 ? polinom[0] : polinom[0] == 0 ? 1 : 0;
+                polinom[2] = int.Parse(vector[2].ToString()) == 0 ? polinom[0] : polinom[0] == 0 ? 1 : 0;
+                polinom[3] = int.Parse(vector[1].ToString()) == 0 ? polinom[0] : polinom[0] == 0 ? 1 : 0;
+
+                polinom[4] = int.Parse(vector[6].ToString()) == 0 ? (polinom[0] + polinom[1] + polinom[2]) % 2 == 0 ? 0 : 1 : (polinom[0] + polinom[1] + polinom[2]) % 2 == 1 ? 0 : 1; if (polinom[4] == 1) return false;
+                polinom[5] = int.Parse(vector[5].ToString()) == 0 ? (polinom[0] + polinom[1] + polinom[3]) % 2 == 0 ? 0 : 1 : (polinom[0] + polinom[1] + polinom[3]) % 2 == 1 ? 0 : 1; if (polinom[5] == 1) return false;
+                polinom[6] = int.Parse(vector[3].ToString()) == 0 ? (polinom[0] + polinom[2] + polinom[3]) % 2 == 0 ? 0 : 1 : (polinom[0] + polinom[2] + polinom[3]) % 2 == 1 ? 0 : 1; if (polinom[6] == 1) return false;
+
+                polinom[7] = int.Parse(vector[7].ToString()) == 0 ? (polinom[0] + polinom[1] + polinom[2] + polinom[3] + polinom[4] + polinom[5] + polinom[6] + polinom[7]) % 2 == 0 ? 0 : 1 : (polinom[0] + polinom[1] + polinom[2] + polinom[3] + polinom[4] + polinom[5] + polinom[6] + polinom[7]) % 2 == 1 ? 0 : 1;
+                if (polinom[7] == 1) return false;
+                else return true;
+
+        }
+        return false;
+
+    }
+    public static string[] ReadIntoFile(string path)
+    {
+        string[] lines = File.ReadAllLines(path);
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string vector = Regex.Replace(lines[i], "[^01]", "");
+            while (!IsCheckGradeTwo(vector.Length))
+            {
+                if (vector.Length == 0) { vector = "0"; break; }
+                vector = vector.Remove(vector.Length - 1);
+            }
+            lines[i] = vector;
+        }
+        return lines;
+    }
+    public static bool IsCheckGradeTwo(int number)
+    {
+        if (number == 0) return false;
+        string s = Convert.ToString(number, 2);
+        if (Regex.IsMatch(s, "^\\b1{1}0{0,3}$"))
+            return true;
+        else return false;
+    }
+}
---
+++ MatrixProperties/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.IO;
+
+namespace MatrixProperties
+{
+    public class Program
+    {
+        static int[,] matrix;
+        static string path = "m1.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*Анализ матрицы отношений*");
+            for (int i = 1; i <= 8; i++)
+            {
+                path = "m" + i.ToString() + ".txt";
+                CheckAll(path);
+                Console.WriteLine("Нажмите любую кнопку для продолжения.\nДля выхода нажмите esc");
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    Environment.Exit(0);
+            }
+            //ReadMatrix(path);
+            //WriteMatrix();
+            //Console.WriteLine(CheckConnectivity());
+
+        }
+        public static void CheckAll(string path)
+        {
+            ReadMatrix(path);
+            Console.WriteLine("\n_____________\n" + path + "\n_____________\n");
+            WriteMatrix();
+            Console.WriteLine("__________________________________________\n");
+            string refl = CheckReflexivity();
+            Console.WriteLine("Рефлективность: \t" + refl);
+            string sym = CheckSymmetry();
+            Console.WriteLine("Симметричность: \t" + sym);
+            string tran = CheckTransitive();
+            Console.WriteLine("Транзитивность: \t" + tran);
+            string equi = CheckEquivalence(refl, sym, tran);
+            Console.WriteLine("Эквивалентность:\t" + equi);
+            string conn = CheckConnectivity();
+            Console.WriteLine("Связность:\t\t" + conn);
+            string ord = CheckOrder(refl, sym, tran, conn);
+            Console.WriteLine("Порядок:\t\t" + ord);
+            Console.WriteLine("\n__________________________________________\n");
+            Console.WriteLine(@"\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+        }
+        public static string CheckOrder(string refl, string sym, string tran, string conn)
+        {
+            if (sym == "Антисимметричное" && tran == "Транзитивное")
+            {
+                if (refl == "Рефлексивное")
+                {
+                    if (conn == "Связное") return "Нестрогое, полное";
+                    else return "Нестрогое, частичное";
+                }
+                else
+                {
+                    if (conn == "Связное") return "Строгое, полное";
+                    else return "Строгое, частичное";
+                }
+            }
+            return "Порядок не имеет";
+        }
+        public static string CheckConnectivity()
+        {
+            bool isConnectivity = true;
+            for (int i = 0; i < matrix.GetLength(0) && isConnectivity; i++)
+                for (int j = i + 1; j < matrix.GetLength(1); j++)
+                {
+                    if (!(matrix[i, j] == 1 || matrix[j, i] == 1))
+                        isConnectivity = false;
+                }
+            if (isConnectivity) return "Связное";
+            else return "Не связное";
+        }
+
+        public static string CheckEquivalence(string refl, string sym, string tran)
+        {
+            if (refl == "Рефлексивное" && sym == "Симметричное" && tran == "Транзитивное")
+                return "Эквивалентное";
+            else return "Не эквивалентное";
+
+        }
+        public static string CheckTransitive()
+        {
+            bool isTransitive = true;
+            for (int i = 0; i < matrix.GetLength(0) && isTransitive; i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1) && isTransitive; j++)
+                {
+                    for (int k = 0; k < matrix.GetLength(1) && isTransitive; k++)
+                    {
+                        if (matrix[i, j] == 1 && matrix[j, k] == 1 && matrix[i, k] != 1)
+                        {
+                            isTransitive = false;
+                            return "Не транзитивное";
+                        }
+                    }
+                }
+            }
+            if (isTransitive) { return "Транзитивное"; }
+            return "";
+        }
+        public static void ReadMatrix(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            matrix = new int[lines.GetLength(0), (lines[0].Length + 1) / 2];
+            for (int i = 0; i < 6; i++)
+            {
+                string[] buf = lines[i].Split(' ');
+                for (int j = 0; j < 6; j++)
+                {
+                    matrix[i, j] = int.Parse(buf[j]);
+                }
+            }
+        }
+        public static void WriteMatrix()
+        {
+            if (matrix == null) { Console.WriteLine("Матрица пуста. "); return; }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+        public static string CheckReflexivity()
+        {
+            bool isReflexivity = true;
+            bool isNotReflexivity = true;
+            for (int i = 0, j = 0; i < matrix.GetLength(0) && (isReflexivity || isNotReflexivity); i++, j++)
+            {
+                if (!(matrix[i, j] == 1 && isReflexivity)) isReflexivity = false;
+                if (!(matrix[i, j] == 0 && isNotReflexivity)) isNotReflexivity = false;
+            }
+            if (isReflexivity) { return "Рефлексивное"; }
+            else if (isNotReflexivity) { return "Aнтирефлексивное"; }
+            else { return "Частично рефлексивное"; }
+        }
+        public static string CheckSymmetry()
+        {
+            bool isSymmetry = true;
+            bool isAntiSymmetry = true;
+            for (int i = 0; i < matrix.GetLength(0) && (isSymmetry || isAntiSymmetry); i++)
+            {
+                for (int j = i + 1; j < matrix.GetLength(1) && (isSymmetry || isAntiSymmetry); j++)
+                {
+                    if (!(matrix[i, j] * matrix[j, i] == matrix[i, j])) isSymmetry = false;
+                    if (!(matrix[i, j] * matrix[j, i] == 0)) isAntiSymmetry = false;
+                }
+            }
+            string isReflexivity = CheckReflexivity();
+            if (isSymmetry) { return "Симметричное"; }
+            else if (isAntiSymmetry && isReflexivity != "Антирефлексивное")
+            { return "Антисимметричное"; }
+            else if (isAntiSymmetry && isReflexivity == "Антирефлексивное")
+            { return "Асимметричное"; }
+            else return "Неявно Симметричное";
+        }
+    }
+}
